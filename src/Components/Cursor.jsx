a5467ad6b2ec2fda46c5d04c413ace1432f433ff
resulcelik <>
2024-08-17 22:52:42 +0300

    v1.15

--- src/Components/Cursor.jsx
+++ src/Components/Cursor.jsx
@@ -1,6 +1,5 @@
 const Cursor = ({line,cursor, direction}) => {
 
-
     return (
         <>
             {
@@ -8,8 +7,8 @@ const Cursor = ({line,cursor, direction}) => {
                 (
                   <div className='line line-horizontal' style={{top: '50%', left: 0}} ref={line}>
                       <div className="cursor" style={{top: 'calc(50% - 25px)', left: '50%'}} ref={cursor}>
-                        <div className="dashed"></div>
-                        <div className="dashed"></div>
+                      {direction == 'u' | direction == 'v' ? <div className="dashes d-u"></div> : <></>}
+                      {direction == 'd' | direction == 'v' ? <div className="dashes d-d"></div> : <></>}
                       </div>
                   </div>
                 )
@@ -17,8 +16,8 @@ const Cursor = ({line,cursor, direction}) => {
                 (
                   <div className='line' style={{top: 0,left: '50%'}} ref={line}>
                       <div className="cursor" style={{top: '50%', left: 'calc(50% - 25px)'}} ref={cursor}>
-                        <div className="dashed"></div>
-                        <div className="dashed"></div>
+                        {direction == 'l' | direction == 'h' ? <div className="dashes d-l"></div> : <></>}
+                        {direction == 'r' | direction == 'h' ? <div className="dashes d-r"></div> : <></>}
                       </div>
                   </div>
                 )
